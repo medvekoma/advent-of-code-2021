5731386ea2cbf9a1cf29848b0aa5b15e709a3f72
Attila Szucs <attila.szucs@gmail.com>
2021-12-09 17:53:04 +0100

    optimize day 9

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -9,33 +9,31 @@ object Day09 extends App {
       .toList
   }.get
 
-  class Board(input: List[List[Int]]) {
+  class Board(board: List[List[Int]]) {
 
-    // create board surrounded with 9s to avoid testing for out of bounds
-    private val board = {
-      val cols = input.headOption.getOrElse(List()).size
-      val nineRow = List.fill(cols + 2)(9)
-      nineRow +: input.map(list => 9 +: list :+ 9) :+ nineRow
-    }
+    val rows: Int = input.size
+    val cols: Int = input.headOption.getOrElse(List.empty).size
 
-    def neighbours(row: Int, col: Int): List[(Int, Int)] =
-      List(
-        (row-1, col),
-        (row+1, col),
-        (row, col-1),
-        (row, col+1)
-      )
+    def neighbours(row: Int, col: Int): Seq[(Int, Int)] =
+      Seq(
+        (row - 1, col),
+        (row + 1, col),
+        (row, col - 1),
+        (row, col + 1)
+      ).filter {
+        case (r, c) => (0 until rows).contains(r) && (0 until cols).contains(c)
+      }
 
-    def findLowPoints(): Seq[(Int, Int, Int)] = {
+    def findLowPoints(): Seq[(Int, Int, Int)] =
       for (
-        row <- 1 until board.size - 1;
-        col <- 1 until board(row).size - 1;
+        row <- board.indices;
+        col <- board(row).indices;
         neighbourValues = neighbours(row, col).map { case (r, c) => board(r)(c) };
-        value = board(row)(col) if neighbourValues.forall(value < _)
+        value = board(row)(col)
+        if neighbourValues.forall(_ > value)
       ) yield (value, row, col)
-    }
 
-    def collectBasin(basin: Set[(Int, Int)], collected: Set[(Int, Int)] = Set()): Set[(Int, Int)] = {
+    def collectBasin(basin: Set[(Int, Int)], collected: Set[(Int, Int)] = Set.empty): Set[(Int, Int)] = {
       val allNeighbours = basin
         .flatMap { case (r, c) => neighbours(r, c) }
         .filter { case (r, c) => board(r)(c) < 9 }
@@ -54,7 +52,7 @@ object Day09 extends App {
   println(s"part 1: ${lowValues.sum + lowValues.size}")
 
   val largestBasins = lowPoints
-    .map { case (_, r, c) => board.collectBasin(Set((r, c))).size}
+    .map { case (_, r, c) => board.collectBasin(Set((r, c))).size }
     .sortBy(identity)(Ordering.Int.reverse)
     .take(3)
   println(s"part 2: ${largestBasins.product}")
