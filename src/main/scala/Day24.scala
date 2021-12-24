86737045514695b764f387ec06fd9f153a93e5c6
Attila Szucs <attila.szucs@gmail.com>
2021-12-24 10:39:12 +0100

    works, no input

--- src/main/scala/Day24.scala
+++ src/main/scala/Day24.scala
@@ -18,62 +18,70 @@ object Day24 extends App {
 
   case class Inp(reg: Char, index: Int) extends Expression
 
-  case class Add(reg: Char, a: Expression, b: Expression) extends Expression
+  sealed trait Operation extends Expression {
+    val reg: Char
+    val a: Expression
+    val b: Expression
+  }
 
-  case class Mul(reg: Char, a: Expression, b: Expression) extends Expression
-
-  case class Div(reg: Char, a: Expression, b: Expression) extends Expression
-
-  case class Mod(reg: Char, a: Expression, b: Expression) extends Expression
-
-  case class Eql(reg: Char, a: Expression, b: Expression) extends Expression
+  case class Add(reg: Char, a: Expression, b: Expression) extends Operation
+  case class Mul(reg: Char, a: Expression, b: Expression) extends Operation
+  case class Div(reg: Char, a: Expression, b: Expression) extends Operation
+  case class Mod(reg: Char, a: Expression, b: Expression) extends Operation
+  case class Eql(reg: Char, a: Expression, b: Expression) extends Operation
 
   object Expression {
-    def from(op: String, a: Expression, b: Expression): Expression =
-      op match {
-        case "add" => Add(' ', a, b)
-        case "mul" => Add(' ', a, b)
-        case "div" => Add(' ', a, b)
-        case "mod" => Add(' ', a, b)
-        case "eql" => Add(' ', a, b)
+    def from(op: String, reg: Char, a: Expression, b: Expression): Expression = {
+      (op, b) match {
+        case ("mul", Number(0)) => Number(0)
+        case ("add", _) => Add(reg, a, b)
+        case ("mul", _) => Mul(reg, a, b)
+        case ("div", _) => Div(reg, a, b)
+        case ("mod", _) => Mod(reg, a, b)
+        case ("eql", _) => Eql(reg, a, b)
       }
+    }
   }
 
   case class Line(op: String, reg: Char, reg2: Option[Char], value: Option[Int])
 
   val expressionPattern = "(\\w+) ([wxyz]) ([wxyz]?)(-?\\d*)".r
-  val reverseLines = expandedLines.reverse.collect {
+  val reverseLines = expandedLines.collect {
     case expressionPattern(op, reg, reg2, value) => Line(op, reg.head, reg2.headOption, value.toIntOption)
   }
 
   def findExpression(from: Int, reg: Char): Expression = {
-    //    println(s" -- $from, $reg")
-    if (from >= reverseLines.length)
-      return Number(0)
-    val remainingLines = reverseLines.zipWithIndex.drop(from)
-    val result = remainingLines.find { case (line, _) => line.reg == reg } match {
+    val remainingLines = reverseLines.zipWithIndex.take(from)
+    val result = remainingLines.findLast { case (line, _) => line.reg == reg } match {
       case None => Number(0)
       case Some((line, index)) =>
-        if (index < from)
-          println(s"ERROR: $line, $from, $index")
         line match {
           case Line("inp", _, None, Some(number)) =>
-            println(s"--> Input $reg, $number, $index")
             Inp(reg, number)
-          case Line(op, _, None, Some(number)) =>
-            val a = findExpression(index + 1, reg)
+          case Line(op, regA, None, Some(number)) =>
+            val a = findExpression(index - 1, regA)
             val b = Number(number)
-            Expression.from(op, a, b)
-          case Line(op, _, Some(reg2), None) =>
-            val a = findExpression(index + 1, reg)
-            val b = findExpression(index + 1, reg2)
-            Expression.from(op, a, b)
+            Expression.from(op, reg, a, b)
+          case Line(op, regA, Some(regB), None) =>
+            val a = findExpression(index - 1, regA)
+            val b = findExpression(index - 1, regB)
+            Expression.from(op, reg, a, b)
         }
     }
-    //    println(s" -- $from, $reg, $result")
     result
   }
 
-  val tree = findExpression(0, 'z')
+  val tree = findExpression(reverseLines.length, 'z')
+
+  def findInp(expression: Expression): Seq[Inp] = {
+    expression match {
+      case inp: Inp => Seq(inp)
+      case op: Operation => findInp(op.a) ++ findInp(op.b)
+      case _ => Seq.empty
+    }
+  }
+
+  println(tree)
+  println(findInp(tree).distinct)
 
 }
