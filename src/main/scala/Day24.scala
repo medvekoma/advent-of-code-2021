1c8111cd3b4ee40f98b1248f4982990a59632468
Attila Szucs <attila.szucs@gmail.com>
2022-01-02 14:56:42 +0100

    Day 24 completed

--- src/main/scala/Day24.scala
+++ src/main/scala/Day24.scala
@@ -1,17 +1,21 @@
-import utils.ResourceFile
-
+import utils.{Measure, ResourceFile}
 import scala.util.Try
 
 object Day24 extends App {
-  val lines = ResourceFile.readLines("day24.txt")
+
   val iterator = (13 to 0 by -1).iterator
 
-  val operations = lines
-    .map(_.split(" "))
-    .map(array => (array(0), array(1), Try(array(2)).getOrElse(iterator.next().toString)))
+  case class Operation(op: String, r1: String, r2: String)
 
-  case class Registers(w: Long, x: Long, y: Long, z: Long) {
-    def get(value: String): Long = value match {
+  val operationBlocks = ResourceFile.readLines("day24.txt")
+    .map(_.split(" "))
+    .map(arr => Operation(arr(0), arr(1), Try(arr(2)).getOrElse(iterator.next().toString)))
+    .sliding(18, 18)
+    .toList
+
+  case class Registers(w: Long, z: Long, x: Long = 0L, y: Long = 0L) {
+
+    def get(reg: String): Long = reg match {
       case "w" => w
       case "x" => x
       case "y" => y
@@ -20,70 +24,67 @@ object Day24 extends App {
     }
 
     def set(reg: String, value: Long): Registers = reg match {
-      case "w" => copy(w = value)
-      case "x" => copy(x = value)
-      case "y" => copy(y = value)
-      case "z" => copy(z = value)
-      case _ => throw new RuntimeException(s"Incorrect registry: $reg")
+      case "w" => this.copy(w = value)
+      case "x" => this.copy(x = value)
+      case "y" => this.copy(y = value)
+      case "z" => this.copy(z = value)
     }
   }
 
-  var cycle = 0L
-
-  def findFirst(line: Int, registers: Registers, inputs: String): Option[String] = {
-    if (line >= operations.size)
-      return if (registers.z == 0L) Some(inputs) else None
-
-    def binary(
-                fn: (Long, Long) => Long,
-                valid1: Long => Boolean = _ => true,
-                valid2: Long => Boolean = _ => true
-              )(
-                a: Long, b: Long,
-              ): Option[Long] = {
-      if (valid1(a) && valid2(b))
-        Some(fn(a, b))
-      else
-        None
-    }
-
-    def add = binary(_ + _) _
-
-    def mul = binary(_ * _) _
-
-    def div = binary(_ + _, valid2 = _ != 0) _
-
-    def mod = binary(_ % _, valid1 = _ >= 0, valid2 = _ > 0) _
-
-    def eql = binary((a, b) => if (a == b) 1L else 0L) _
-
-    val operation = operations(line)
-    cycle += 1
-    if (cycle % 1000000 == 0)
-      println(line, inputs)
-    operation match {
-      case ("inp", reg, _) =>
-        (9 to 1 by -1).iterator
-          .map(i => findFirst(line + 1, registers.set(reg, i.toLong), inputs + i))
-          .find(_.isDefined).flatten
-      case ("add", reg, x) =>
-        add(registers.get(reg), registers.get(x))
-          .flatMap(res => findFirst(line + 1, registers.set(reg, res), inputs))
-      case ("mul", reg, x) =>
-        mul(registers.get(reg), registers.get(x))
-          .flatMap(res => findFirst(line + 1, registers.set(reg, res), inputs))
-      case ("div", reg, x) =>
-        div(registers.get(reg), registers.get(x))
-          .flatMap(res => findFirst(line + 1, registers.set(reg, res), inputs))
-      case ("mod", reg, x) =>
-        mod(registers.get(reg), registers.get(x))
-          .flatMap(res => findFirst(line + 1, registers.set(reg, res), inputs))
-      case ("eql", reg, x) =>
-        eql(registers.get(reg), registers.get(x))
-          .flatMap(res => findFirst(line + 1, registers.set(reg, res), inputs))
-    }
+  def binary(fn: (Long, Long) => Long,
+             valid1: Long => Boolean = _ => true,
+             valid2: Long => Boolean = _ => true
+            )(
+              x1: Long, x2: Long,
+            ): Option[Long] = {
+    if (valid1(x1) && valid2(x2))
+      Some(fn(x1, x2))
+    else
+      None
   }
 
-  val result = findFirst(0, Registers(0, 0, 0, 0), "")
-  println(result)
+  def operation(op: String) = op match {
+    case "add" => binary(_ + _) _
+    case "mul" => binary(_ * _) _
+    case "div" => binary(_ / _, valid2 = _ != 0) _
+    case "mod" => binary(_ % _, valid1 = _ >= 0, valid2 = _ > 0) _
+    case "eql" => binary((a, b) => if (a == b) 1L else 0L) _
+  }
+
+  def calculateZ(operations: Seq[Operation], w: Long, prevZ: Long): Option[Long] = {
+    val lastRegisters = operations.tail.foldLeft(Registers(w, prevZ)) { (regs, line) =>
+      operation(line.op)(regs.get(line.r1), regs.get(line.r2)) match {
+        case None => return None
+        case Some(value) =>
+          regs.set(line.r1, value)
+      }
+    }
+    Some(lastRegisters.z)
+  }
+
+  def calculateBlock(block: Int, prevZs: Map[Long, (String, String)]): Map[Long, (String, String)] = {
+    val res = for (
+      w <- 1L to 9L;
+      (prevZ, (monadMin, monadMax)) <- prevZs;
+      nextZ <- calculateZ(operationBlocks(block), w, prevZ)
+    ) yield (nextZ, (monadMin + w, monadMax + w))
+    res
+      .groupMap(_._1)(_._2)
+      .map { case (nextZ, pair) => (nextZ, (pair.map(_._1).min, pair.map(_._2).max)) }
+  }
+
+  def calculateMonadMinMax(): (String, String) = {
+    var prevZs = Map(0L -> ("", ""))
+    for (block <- operationBlocks.indices) {
+      println(block)
+      prevZs = calculateBlock(block, prevZs)
+    }
+    prevZs(0)
+  }
+
+  val (min, max) = Measure.dumpTime() {
+    calculateMonadMinMax()
+  }
+  println(s"part 1: $max")
+  println(s"part 2: $min")
 }
