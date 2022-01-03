4b5f13e4315cbbe62aee15addf51e9e8eae6c424
Attila Szucs <attila.szucs@gmail.com>
2022-01-03 09:19:04 +0100

    final modifications for Day25

--- src/main/scala/Day25.scala
+++ src/main/scala/Day25.scala
@@ -6,29 +6,26 @@ object Day25 extends App {
 
   val originalLines = ResourceFile.readLines("day25.txt")
 
-  def step(cucumber: Char)(lines: List[String]): List[String] = {
+  val transpose = (lines: List[String]) => lines.transpose.map(_.mkString)
+
+  def stepHorizontally(sign: Char)(lines: List[String]): List[String] =
     lines.map { line =>
-      val text = (line.last + line + line.head).replace(s"$cucumber.", s".$cucumber")
+      val text = (line.last + line + line.head).replace(s"$sign.", s".$sign")
       text.tail.take(line.length)
     }
-  }
 
-  def transpose(lines: List[String]): List[String] = lines.transpose.map(_.mkString)
-
-  def stepEast = step('>') _
-
-  def stepSouth = transpose _ andThen step('v') andThen transpose
-
-  def fullStep = stepEast andThen stepSouth
+  val stepEast = stepHorizontally('>') _
+  val stepSouth = transpose andThen stepHorizontally('v') andThen transpose
+  val step = stepEast andThen stepSouth
 
   @tailrec
-  def evolve(lines: List[String], steps: Int = 0): Int = {
-    val evolvedLines = fullStep(lines)
-    if (evolvedLines == lines)
-      steps + 1
-    else
-      evolve(evolvedLines, steps + 1)
-  }
+  def countSteps(lines: List[String], steps: Int = 0): Int =
+    step(lines) match {
+      case evolvedLines if evolvedLines != lines =>
+        countSteps(evolvedLines, steps + 1)
+      case _ =>
+        steps + 1
+    }
 
-  println(s"part 1: ${evolve(originalLines)}")
+  println(s"part 1: ${countSteps(originalLines)}")
 }
