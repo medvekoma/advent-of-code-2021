a2116b2b2699277a2e9f4ce896859ca6752a1f81
Attila Szucs <attila.szucs@gmail.com>
2022-01-05 09:51:56 +0100

    collect

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -1,6 +1,9 @@
 import utils.ListExtensions.RichList
 import utils.ResourceFile
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 object Day19 extends App {
 
   type Beacon = (Int, Int, Int)
@@ -25,19 +28,18 @@ object Day19 extends App {
     octave.flatMap(spin3)
   }
 
-  def spinMatrix(beacons: Seq[Beacon]): Seq[Seq[Beacon]] =
-    beacons.map(spin)
+  def spinBlock(beacons: Set[Beacon]): Seq[Set[Beacon]] =
+    beacons.map(spin).transpose.toSeq
 
   def toBeacon(string: String): Beacon =
     string.split(',') match {
       case Array(x, y, z) => (x.toInt, y.toInt, z.toInt)
     }
 
-  val lines = ResourceFile.readLines("_day19.txt")
+  val lines = ResourceFile.readLines("day19.txt")
   val blocks = lines
     .splitBySeparator("")
-    .map(_.tail.map(toBeacon))
-  blocks.foreach(block => println(block.size, block))
+    .map(_.tail.map(toBeacon).toSet)
 
   implicit class RichBeacon(beacon: Beacon) {
     def +(other: Beacon): Beacon = (beacon._1 + other._1, beacon._2 + other._2, beacon._3 + other._3)
@@ -45,17 +47,66 @@ object Day19 extends App {
     def -(other: Beacon): Beacon = (beacon._1 - other._1, beacon._2 - other._2, beacon._3 - other._3)
   }
 
-  implicit class RichBeacons(beacons: Seq[Beacon]) {
-    def -(other: Seq[Beacon]): Seq[Beacon] =
-      beacons
-        .zip(other)
-        .map { case (b1, b2) => b1 - b2 }
+  def maxOverlap(block1: Set[Beacon], block2: Set[Beacon]): Option[Set[Beacon]] = {
+    val differences = for (
+      b1 <- block1.iterator;
+      block2v <- spinBlock(block2);
+      b2 <- block2v;
+      diff = b1 - b2;
+      block2m = block2v.map(_ + diff) if (block1 & block2m).size >= 12
+    ) yield block2m
+    differences.nextOption()
   }
 
-  val matrix1 = blocks.head
-  val matrix2 = blocks.tail.head
+  var unified = blocks.head
 
-  matrix2.map(spin)
-    .map(matrix => matrix1 - matrix)
-    .foreach(println)
+  @tailrec
+  def findOverlaps(blocks: Seq[Set[Beacon]]): Unit = {
+    println(s"... ${blocks.size}")
+    if (blocks.nonEmpty) {
+      val (overlaps, noOverlaps) = blocks
+        .map { block => (block, maxOverlap(unified, block)) }
+        .partition(_._2.isDefined)
+      overlaps.foreach(unified ++= _._2.get)
+      findOverlaps(noOverlaps.map(_._1))
+    }
+  }
+
+  val indexedBlocks = blocks.zipWithIndex
+    .map{ case (block, index) => (index, block)}
+    .toMap
+
+  val unifiedBlocks = mutable.HashMap[Int, Set[Beacon]]()
+
+  def unify(blocks: Map[Int, Set[Beacon]], key: Int): Unit = {
+    if (blocks.nonEmpty) {
+      val homeBlock = unifiedBlocks(key)
+      unifiedBlocks += (key -> homeBlock)
+      val foundBlocks = (blocks - key)
+        .map { case (idx, block) => (idx, maxOverlap(homeBlock, block)) }
+        .collect { case (idx, Some(transformedBlock)) => (idx, transformedBlock) }
+      unifiedBlocks ++= foundBlocks
+      foundBlocks.foreach {
+        case (idx, _) =>
+          val remainingMap = indexedBlocks -- unifiedBlocks.keySet
+          println(remainingMap.size)
+          unify(remainingMap, idx)
+      }
+    }
+  }
+
+  do {
+    val remainingBlocks = indexedBlocks -- unifiedBlocks.keySet
+    val key = remainingBlocks.head._1
+    unifiedBlocks += remainingBlocks.head
+    unify(remainingBlocks, key)
+  } while (indexedBlocks.size > unifiedBlocks.size)
+
+  unify(indexedBlocks, 0)
+  println(indexedBlocks.size, unifiedBlocks.size)
+  println(unifiedBlocks.values.reduce(_ ++ _).size)
+
+
+//  findOverlaps(blocks.tail)
+//  println(s"part 1: ${res.size}")
 }
--- src/main/scala/Day25.scala
+++ src/main/scala/Day25.scala
@@ -8,21 +8,21 @@ object Day25 extends App {
 
   val transpose = (lines: List[String]) => lines.transpose.map(_.mkString)
 
-  def stepHorizontally(sign: Char)(lines: List[String]): List[String] =
+  val stepHorizontally = (sign: Char) => (lines: List[String]) =>
     lines.map { line =>
-      val text = (line.last + line + line.head).replace(s"$sign.", s".$sign")
-      text.tail.take(line.length)
+      (line.last + line + line.head)
+        .replace(s"$sign.", s".$sign")
+        .tail.take(line.length)
     }
-
-  val stepEast = stepHorizontally('>') _
+  val stepEast = stepHorizontally('>')
   val stepSouth = transpose andThen stepHorizontally('v') andThen transpose
   val step = stepEast andThen stepSouth
 
   @tailrec
-  def countSteps(lines: List[String], steps: Int = 0): Int =
-    step(lines) match {
-      case evolvedLines if evolvedLines != lines =>
-        countSteps(evolvedLines, steps + 1)
+  def countSteps(prevLines: List[String], steps: Int = 0): Int =
+    step(prevLines) match {
+      case nextLines if nextLines != prevLines =>
+        countSteps(nextLines, steps + 1)
       case _ =>
         steps + 1
     }
