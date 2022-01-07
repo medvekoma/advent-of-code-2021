55a5ce9bec6b99d8cdd5e29ed1e025243d88d4d8
Attila Szucs <attila.szucs@gmail.com>
2022-01-07 15:44:16 +0100

    Speed up Day23 (#9)

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -11,7 +11,17 @@ object Day23 extends App {
 
   type Cell = (Int, Int)
 
-  class Board(matrix: List[List[Char]], val cost: Int = 0) {
+  object Board {
+    def fromMatrix(matrix: List[List[Char]]): Board = {
+      val map = matrix.cells
+        .map(cell => (cell, matrix(cell)))
+        .filter { case (cell, ch) => "ABCD".contains(ch) }
+        .toMap
+      new Board(map, matrix.rows, 0)
+    }
+  }
+
+  class Board(cellMap: Map[Cell, Char], rows: Int, val cost: Int) {
 
     private val costMap: Map[Char, Int] = Map(
       'A' -> 1,
@@ -20,28 +30,25 @@ object Day23 extends App {
       'D' -> 1000
     )
 
-    private val homeRows = (1 until matrix.rows).toList
+    private val homeRows = (1 until rows).toList
     private val homeCols = Map('A' -> 3, 'B' -> 5, 'C' -> 7, 'D' -> 9)
     private val homeMap: Map[Char, List[Cell]] =
       homeCols.map { case (ch, col) => (ch, homeRows.map((_, col))) }
 
     private val restPlaces = Seq(1, 2, 4, 6, 8, 10, 11).map((0, _)).toList
 
-    private def replace(source: Cell, target: Cell): List[List[Char]] = {
-      List.tabulate[Char](matrix.rows, matrix.cols) {
-        case `source` => matrix(target)
-        case `target` => matrix(source)
-        case x => matrix(x)
-      }
+    private def replace(source: Cell, target: Cell): Map[Cell, Char] = {
+      val ch = cellMap(source)
+      cellMap - source + (target -> ch)
     }
 
-    private def pathCells(cell1: Cell, cell2: Cell): Set[Cell] =
-      (cell1._1 until 0 by -1).map(row => (row, cell1._2)).toSet ++
-        (Math.min(cell1._2, cell2._2) to Math.max(cell1._2, cell2._2)).map(col => (0, col)).toSet ++
-        (0 to cell2._1).map(row => (row, cell2._2)).toSet -- Set(cell1)
+    private def pathCells(source: Cell, target: Cell): Set[Cell] =
+      (source._1 - 1 until 0 by -1).map(row => (row, source._2)).toSet ++
+        (Math.min(source._2, target._2) to Math.max(source._2, target._2)).map(col => (0, col)).toSet ++
+        (0 to target._1).map(row => (row, target._2)).toSet -- Set(source)
 
     private def isValidMove(path: Set[Cell]): Boolean =
-      path.forall(cell => matrix(cell) == ' ')
+      (path & cellMap.keySet).isEmpty
 
     trait HomeState
 
@@ -51,18 +58,24 @@ object Day23 extends App {
 
     case class HomeStepIn(toCell: Cell) extends HomeState
 
+    def contentOf(cell: Cell): Char =
+      cellMap.getOrElse(cell, ' ')
+
+    def contentOf(cells: Seq[Cell]): Seq[Char] =
+      cells.map(contentOf)
+
     def homeState(ch: Char): HomeState = {
       val cells = homeMap(ch)
-      val content = contentOf(cells).distinct.sorted.toList
-      content match {
+      val contentList = contentOf(cells).distinct.sorted.toList
+      contentList match {
         case List(`ch`) =>
           HomeReady
         case List(' ', `ch`) =>
-          HomeStepIn(cells.findLast(matrix(_) == ' ').get)
+          HomeStepIn(cells.findLast(contentOf(_) == ' ').get)
         case List(' ') =>
           HomeStepIn(cells.last)
         case _ =>
-          HomeStepOut(cells.find(matrix(_) != ' ').get)
+          HomeStepOut(cells.find(contentOf(_) != ' ').get)
       }
     }
 
@@ -82,39 +95,47 @@ object Day23 extends App {
           None
       }
 
+    def stepInCells: Map[Char, Cell] =
+      homeMap.keySet
+        .map(ch => (ch, stepInCell(ch)))
+        .collect { case (ch, Some(cell)) => (ch, cell) }
+        .toMap
+
     def stepOutCells: Set[Cell] =
       homeMap.keySet
         .flatMap(stepOutCell)
 
-
     def isReady: Boolean =
       homeMap.keySet.forall(ch => homeState(ch) == HomeReady)
 
-    def contentOf(cells: Seq[Cell]): Seq[Char] =
-      cells.map(matrix(_))
-
     def move(source: Cell, target: Cell): Option[Board] =
-      costMap.get(matrix(source)) match {
+      costMap.get(contentOf(source)) match {
         case None => None
         case Some(pieceCost) =>
           val path = pathCells(source, target)
           if (isValidMove(path))
-            Some(new Board(replace(source, target), cost + pieceCost * path.size))
+            Some(new Board(replace(source, target), this.rows, cost + pieceCost * path.size))
           else
             None
       }
 
-    def emptyRestPlaces: Seq[Cell] =
-      restPlaces.filter(matrix(_) == ' ')
+    def emptyRestPlaces(cell: Cell): Seq[Cell] = {
+      val (before, after) = restPlaces.partition(rest => rest._2 < cell._2)
+      before.reverse.takeWhile(contentOf(_) == ' ') ++ after.takeWhile(contentOf(_) == ' ')
+    }
 
     def occupiedRestPlaces: Seq[Cell] =
-      restPlaces.filter(matrix(_) != ' ')
+      restPlaces.filter(contentOf(_) != ' ')
+
+    def occupiedRestPlaces(cell: Cell): Seq[Cell] = {
+      val (before, after) = occupiedRestPlaces.partition(rest => rest._2 < cell._2)
+      (before.lastOption ++ after.headOption).toSeq
+    }
 
     def firstStepIn(): Option[Board] = {
       val possibleRuns = for (
-        source <- (occupiedRestPlaces ++ stepOutCells).iterator;
-        ch = matrix(source);
-        target <- stepInCell(ch);
+        (ch, target) <- stepInCells.iterator;
+        source <- occupiedRestPlaces(target) ++ stepOutCells if contentOf(source) == ch;
         board <- move(source, target)
       ) yield board
       possibleRuns.nextOption()
@@ -123,37 +144,42 @@ object Day23 extends App {
     def allStepOuts(): Seq[Board] = {
       val boards = for (
         source <- stepOutCells;
-        target <- emptyRestPlaces;
+        target <- emptyRestPlaces(source);
         board <- move(source, target)
       ) yield board
       boards.toSeq
     }
 
-    override def toString: String =
+    override def toString: String = {
+      val cols = restPlaces.map(_._2).last
+      val matrix = List.tabulate(rows, cols)((r, c) => contentOf((r, c)))
       cost.toString + matrix.map(_.mkString).mkString("\n", "\n", "\n")
-  }
-
-  def findMinimumCost(board: Board): Option[Int] =
-    if (board.isReady)
-      Some(board.cost)
-    else board.firstStepIn() match {
-      case Some(newBoard) =>
-        findMinimumCost(newBoard)
-      case None =>
-        board.allStepOuts()
-          .flatMap(newBoard => findMinimumCost(newBoard))
-          .minOption
     }
 
-  val board2 = new Board(matrix)
-  val board1 = new Board(matrix.take(2) ++ matrix.drop(4))
-
-  println("This will take about two minutes ...")
-  Measure.dumpTime() {
-    println(s"part 1: ${findMinimumCost(board1)}")
+    def findMinimumCost(): Option[Int] = {
+//      println(this)
+      if (isReady)
+        Some(cost)
+      else firstStepIn() match {
+        case Some(board) =>
+          board.findMinimumCost()
+        case None =>
+          allStepOuts()
+            .flatMap(board => board.findMinimumCost())
+            .minOption
+      }
+    }
   }
-  println("Another minute to go ...")
+
+  val board2 = Board.fromMatrix(matrix)
+  val board1 = Board.fromMatrix(matrix.take(2) ++ matrix.drop(4))
+
+  println("This will take about a minute ...")
   Measure.dumpTime() {
-    println(s"part 2: ${findMinimumCost(board2)}")
+    println(s"part 1: ${board1.findMinimumCost()}")
+  }
+  println("Please hold on ...")
+  Measure.dumpTime() {
+    println(s"part 2: ${board2.findMinimumCost()}")
   }
 }
